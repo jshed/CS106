cbea032c0304f7dcc683dcf83cca5a8a4e6aa702
Shedrofsky <jshedrof@cslab15.cs.skidmore.edu>
2014-09-03 20:06:16 -0400

    initial commit

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author jshedrof
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("   _     _");
+		System.out.println("   ^     ^ ");
+		System.out.println("(     ^     )");
+		System.out.println("    ______  ");
+
+	}
+
+}
