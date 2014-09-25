17f1c614b364ea05dabd8292e13a221ee55b424a
jshed <Jordan@jordans-mbp.skidmore.edu>
2014-09-24 20:24:29 -0400

    Project Two
    Which Breaking Bad Character Are You

---
+++ Smiley Face/src/SmileyFace.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class SmileyFace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DrawingPanel panel = new DrawingPanel(500, 500);
+	}
+
+}
